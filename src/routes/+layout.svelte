5b8ae7b2e56cac448393a2b9979223708ac852eb
designerwee <designerwee@gmail.com>
2024-06-27 00:44:01 +0900

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.css';
+	import 'remixicon/fonts/remixicon.css';
+</script>
+
+<style lang="postcss">
+   :global(body) {
+      @apply bg-dark text-light font-inter
+   }
+</style>
+
+<main>
+	<slot />
+</main>
